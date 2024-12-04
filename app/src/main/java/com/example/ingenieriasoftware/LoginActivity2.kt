77353dc1d30090494eeae08f1ff77e5c57dcf398
HirepanB <bernabe18hire@gmail.com>
2024-12-04 17:48:48 -0600

    prueba cambio de color

--- app/src/main/java/com/example/ingenieriasoftware/LoginActivity2.kt
+++ app/src/main/java/com/example/ingenieriasoftware/LoginActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.ingenieriasoftware
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -22,14 +23,20 @@ class LoginActivity2 : AppCompatActivity() {
             val username = etUsername.text.toString()
             val password = etPassword.text.toString()
 
-            // Validación simple (puedes agregar validaciones más complejas según sea necesario)
-            if (username.isNotEmpty() && password.isNotEmpty()) {
-                // Lógica de inicio de sesión (aquí puedes validar las credenciales, por ejemplo)
-                Toast.makeText(this, "Bienvenido, $username", Toast.LENGTH_SHORT).show()
-            } else {
-                // Muestra un mensaje de error si los campos están vacíos
-                Toast.makeText(this, "Por favor, ingrese usuario y contraseña", Toast.LENGTH_SHORT).show()
+            // Establece el OnClickListener para abrir LoginActivity2
+            btnLogin.setOnClickListener {
+                val intent = Intent(this, menuPrincipal::class.java)
+                startActivity(intent) // Abre LoginActivity2
             }
+
+//            // Validación simple (puedes agregar validaciones más complejas según sea necesario)
+//            if (username.isNotEmpty() && password.isNotEmpty()) {
+//                // Lógica de inicio de sesión (aquí puedes validar las credenciales, por ejemplo)
+//                Toast.makeText(this, "Bienvenido, $username", Toast.LENGTH_SHORT).show()
+//            } else {
+//                // Muestra un mensaje de error si los campos están vacíos
+//                Toast.makeText(this, "Por favor, ingrese usuario y contraseña", Toast.LENGTH_SHORT).show()
+//            }
         }
     }
 }
